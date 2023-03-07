ab97023b5aab732f7ecab40dcc9889be477d842c
heitorgb <heitorpet1@gmail.com>
2023-03-07 17:57:30 -0300

    adicionado css, cirado head e header

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
     # return HTTP RESPONSE
